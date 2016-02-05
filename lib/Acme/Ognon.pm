1f6d2b484989eb132e9ba015b793dd58d399cbd0
Lee Johnson <lee@givengain.ch>
2016-02-05 19:14:37 +0100

    complet! encore

--- lib/Acme/Ognon.pm
+++ lib/Acme/Ognon.pm
@@ -41,7 +41,7 @@ use vars qw/ @ISA @EXPORT_OK @EXPORT /;
 @ISA       = qw/ Exporter /;
 @EXPORT    = qw/ ognon oignon /;
 
-$Acme::Ognon::VERSION = 1.00
+$Acme::Ognon::VERSION = "1.00";
 
 sub ognon {
 	my ( $text ) = @_;
